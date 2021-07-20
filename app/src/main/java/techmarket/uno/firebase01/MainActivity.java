93d1f91ec840d3cb380eb58a16428792bf946021
chigovv <chigovv@gmail.com>
2021-07-20 16:52:42 +0300

    first user!!!

--- app/src/main/java/techmarket/uno/firebase01/MainActivity.java
+++ app/src/main/java/techmarket/uno/firebase01/MainActivity.java
@@ -3,12 +3,43 @@ package techmarket.uno.firebase01;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText etName, etLastName, etEmail;
+    private DatabaseReference databaseReference;
+    private String USER_KEY = "user";
+    private User newUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+    }
+
+    public void onClickSave(View view) {
+        String id = databaseReference.getKey();
+        String name = etName.getText().toString();
+        String sec_name = etLastName.getText().toString();
+        String email = etEmail.getText().toString();
+        newUser = new User(id,name,sec_name,email);
+        databaseReference.push().setValue(newUser);
+    }
+
+    public void onClickRead(View view) {
+    }
+
+    private void init()
+    {
+        etName = findViewById(R.id.etName);
+        etLastName = findViewById(R.id.etLastName);
+        etEmail = findViewById(R.id.etEmail);
+        databaseReference = FirebaseDatabase.getInstance().getReference(USER_KEY);
     }
 }
